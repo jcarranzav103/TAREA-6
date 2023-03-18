fd28439632992e4c3dd126a41a55800cd8b67228
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-03-18 16:26:32 -0600

    update

--- assets/js/calculo.js
+++ assets/js/calculo.js
@@ -4,15 +4,21 @@ function mostrarValor() {
 }
 
 function montoPago() {
-    var miModal = new bootstrap.Modal(document.getElementById('miModal'));
+    var campoEntrada = document.getElementById('monthlyPayment');
 
-    var hv=document.getElementById("homeValue").value;  //Valor de la vivienda
-    var ms = document.getElementById("amountRequired").value; //Monto solicitado
-    var tm = document.getElementById("interestRate").value; //Tasa de interés mensual
-    var p = document.getElementById("rango").value * 12; //Plazo en meses
+    var hv= parseFloat(document.getElementById("homeValue").value);  //Valor de la vivienda
+    var ms = parseFloat(document.getElementById("amountRequired").value); //Monto solicitado
+    var tm = parseFloat(document.getElementById("interestRate").value); //Tasa de interés mensual
+    var p = parseInt(document.getElementById("rango").value * 12); //Plazo en meses
 
     var pm = ms*((tm/100) * Math.pow((1+(tm/100)),p)) / (Math.pow((1+(tm/100)),p)- 1); //Pago mensual
-    miModal.show();
-    document.getElementById("monthlyPayment").innerHTML = valor;
+
+    if (pm>0) {
+        campoEntrada.value=pm;
+        miModal.show();  
+         event.preventDefault();
+    } 
+   
+    
 
 } 
